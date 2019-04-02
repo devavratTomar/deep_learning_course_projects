3e90b7350b2e25023e0da8979faea389321de550
Devavrat Tomar <devavrat694@gmail.com>
2019-04-02 18:45:32 +0200

    Relu, seq, linear, tanh

--- Proj2/strawberry/Module/Tanh.py
+++ Proj2/strawberry/Module/Tanh.py
@@ -1,4 +1,21 @@
+import torch
 from .base_module import BaseModule
 
 class Tanh(BaseModule):
-    pass
+    def __init__(self):
+        # Storting where inputs is greater than zero
+        self.data = None
+
+    def forward(self, inputs):
+        """
+        Apply Tanh activation on inputs.
+        """
+        self.data = torch.tanh(inputs)
+        return self.data
+
+    def backward(self, gradwrtoutput):
+        gradin = 1.0 - self.data*self.data
+        return gradin*gradwrtoutput
+
+    def param(self):
+        return []
