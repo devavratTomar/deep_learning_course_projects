fbc1280c6494976d9d5f45371e7f93b7d3d5ad5d
Manana <mananikolordkipanidze@gmail.com>
2019-04-07 17:57:09 +0200

    Finished CrossEntropy

--- Proj2/strawberry/Loss/CrossEntropy.py
+++ Proj2/strawberry/Loss/CrossEntropy.py
@@ -20,48 +20,15 @@ class CrossEntropy(BaseModule):
         
         N = inputs.shape[0] if self.reduce else 1.
         
-        #print("targets:")
-        #print(targets)
-        
-        #print("input:")
-        #print(inputs)
-        
-        #print("true log softmax:")
-        #print(inputs.softmax(1))
-        
-                
-        #inputs.requires_grad_()
-        
         max_inp = inputs.max(1)[0]    
         #substract max from logits for numerical efficiency
         inputs_norm = (inputs.t() - max_inp.view(1, -1)).t()
         log_softmax = (inputs_norm.t() - torch.log(torch.exp(inputs_norm).sum(dim=1))).t()
 
-        
-        #print("my log softmax:")
-        #print(log_softmax)
-        
-        
-        loss = torch.nn.NLLLoss()
-        #print("true loss")
-        
-        input_tens = Variable(inputs.data, requires_grad=True)
-        
-        with torch.enable_grad():
-            output = loss(input_tens.log_softmax(1), targets.max(1)[1])
-        #print(output)
-        output.backward()
-       
-        
-        #print("my loss")
-        ce_loss = -(log_softmax * targets).sum()/N
-        #print(ce_loss)
-        
-        #print("true_gradient:")
-        #print(input_tens.grad)
-        
-        self.inputs = torch.softmax(inputs, 1)
-        self.targets=targets
+        ce_loss = -(log_softmax * targets).sum()/N   
+    
+        self.softmax = (inputs_norm.exp().t() / inputs_norm.exp().sum(dim=1)).t()
+        self.targets = targets
         
         return ce_loss
 
@@ -69,11 +36,9 @@ class CrossEntropy(BaseModule):
         """
         Computes gradients with respect to the input of this module and returns it
         """
-        N = self.inputs.shape[0] if self.reduce else 1.
-        
-        #print((-self.targets + self.inputs)/N)
-        
-        return -self.targets + self.inputs
+        N = self.softmax.shape[0] if self.reduce else 1.
+                
+        return (-self.targets + self.softmax)/N
         
         
     
