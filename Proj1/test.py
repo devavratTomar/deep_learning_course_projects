c11ee58385be7d0c8e24ff13eeb2a4ffff238df0
Devavrat Tomar <devavrat694@gmail.com>
2019-05-16 15:23:36 +0200

    adding history to lists

--- Proj1/test.py
+++ Proj1/test.py
@@ -16,6 +16,12 @@ test_accuracies = {
 }
 
 
+history_mlp_net = []
+history_conv_net = []
+history_conv_net_aux = []
+history_siamese = []
+history_siamese_aux = []
+
 for n_run in range(N_RUNS):
     data_set = generate_pair_sets(N_POINTS)
     MAX_VAL = 255.0
@@ -32,8 +38,8 @@ for n_run in range(N_RUNS):
     ##############################################################################
     mlp_net = MLPNet(in_features=392, out_features=2, n_layers=3, n_hidden=16)
     mlp_net.train()
-    history_mlp_net = train_model(mlp_net, train_input=TRAIN_INPUT.view((N_POINTS, -1)), train_target=TRAIN_TARGET, aux_param=1.0,
-                                  val_input=TEST_INPUT.view((N_POINTS, -1)), val_target=TEST_TARGET)
+    history_mlp_net.append(train_model(mlp_net, train_input=TRAIN_INPUT.view((N_POINTS, -1)), train_target=TRAIN_TARGET, aux_param=1.0,
+                                       val_input=TEST_INPUT.view((N_POINTS, -1)), val_target=TEST_TARGET))
     
     mlp_net.eval()
     acc = get_accuracy(mlp_net, TEST_INPUT.view((N_POINTS, -1)), TEST_TARGET)*100.0
@@ -42,8 +48,8 @@ for n_run in range(N_RUNS):
     ##############################################################################
     conv_net = ConvNet(n_classes=2, n_layers=3, n_features=16)
     conv_net.train()
-    history_conv_net = train_model(conv_net, train_input=TRAIN_INPUT, train_target=TRAIN_TARGET, aux_param=1.0,
-                                   val_input=TEST_INPUT, val_target=TEST_TARGET)
+    history_conv_net.append(train_model(conv_net, train_input=TRAIN_INPUT, train_target=TRAIN_TARGET, aux_param=1.0,
+                                        val_input=TEST_INPUT, val_target=TEST_TARGET))
     
     conv_net.eval()
     acc = get_accuracy(conv_net, TEST_INPUT, TEST_TARGET)*100.0
@@ -53,8 +59,8 @@ for n_run in range(N_RUNS):
     ##############################################################################
     conv_net_aux = ConvNet(n_classes=22, n_layers=3, n_features=16)
     conv_net_aux.train()
-    history_conv_net_aux = train_model(conv_net_aux, train_input=TRAIN_INPUT, train_target=TRAIN_TARGET, aux_param=1.0,
-                                       train_classes=TRAIN_CLASSES, val_input=TEST_INPUT, val_target=TEST_TARGET, val_classes=TEST_CLASSES)
+    history_conv_net_aux.append(train_model(conv_net_aux, train_input=TRAIN_INPUT, train_target=TRAIN_TARGET, aux_param=1.0,
+                                            train_classes=TRAIN_CLASSES, val_input=TEST_INPUT, val_target=TEST_TARGET, val_classes=TEST_CLASSES))
     
     conv_net_aux.eval()
     acc = get_accuracy(conv_net_aux, TEST_INPUT, TEST_TARGET)*100.0
@@ -67,8 +73,8 @@ for n_run in range(N_RUNS):
     
     conv_net.train()
     conv_net_siamese.train()
-    history_siamese = train_model(conv_net_siamese, train_input=TRAIN_INPUT, train_target=TRAIN_TARGET,
-                                  val_input=TEST_INPUT, val_target=TEST_TARGET)
+    history_siamese.append(train_model(conv_net_siamese, train_input=TRAIN_INPUT, train_target=TRAIN_TARGET,
+                                       val_input=TEST_INPUT, val_target=TEST_TARGET))
     
     conv_net.eval()
     conv_net_siamese.eval()
@@ -83,8 +89,8 @@ for n_run in range(N_RUNS):
     
     conv_net.train()
     conv_net_siamese_aux.train()
-    history_siamese = train_model(conv_net_siamese_aux, train_input=TRAIN_INPUT, train_target=TRAIN_TARGET, train_classes=TRAIN_CLASSES,
-                                  val_input=TEST_INPUT, val_target=TEST_TARGET, val_classes=TEST_CLASSES, aux_param=3.0, verbose=False)
+    history_siamese_aux.append(train_model(conv_net_siamese_aux, train_input=TRAIN_INPUT, train_target=TRAIN_TARGET, train_classes=TRAIN_CLASSES,
+                                           val_input=TEST_INPUT, val_target=TEST_TARGET, val_classes=TEST_CLASSES, aux_param=3.0, verbose=False))
     
     conv_net.eval()
     conv_net_siamese_aux.eval()
