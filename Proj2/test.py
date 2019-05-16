4b0373fec7cc57b6a1d4b400fc4665f1a9771310
Manana <mananikolordkipanidze@gmail.com>
2019-05-16 23:06:24 +0200

    comments/ bug fix

--- Proj2/test.py
+++ Proj2/test.py
@@ -12,57 +12,75 @@ import helpers as h
 
 # testing linear module
 def test_linear():
+    """
+    Testes Linear Layer with dummy prediction problem
+    """
     W = torch.tensor([[5], [1], [-6], [7], [8], [-2]], dtype=torch.float)
     B = -3
     
     # create dummy data with noisy labels
     TRAIN_FEATURES = torch.empty(1000, 6).uniform_(-1, 1)
     
-    #labels are either one or minus one
+    # compute labels as Wx+b plus some gaussian noise
     TRAIN_LABELS = torch.mm(TRAIN_FEATURES, W) + B + torch.empty((1000, 1)).normal_(0, 0.5)
 
+    # build a sequential model with 6 input features and one output
     model = Module.Sequential(Module.Linear(in_features=6, out_features=1))
+    
+    # use MSE as a kiss function
     loss_fun = Loss.MSE()
+    
+    #use SGD optimizer
     opt = Optimizer.SGD(lr=0.01)
     
+    # train our model given our data for 100 epochs with 10 batch size
     model.train(TRAIN_FEATURES, TRAIN_LABELS, epochs=100, verbose=False,
                 loss=loss_fun, opt=opt, batch_size=10)
     
-    print("True weights: {}, {}".format(W, B))
+    print("True weights: {}, {}".format(W, B)) #true weights without noise
     print("Learned weights: {}, {}".format(model.param()[0][0], 
           model.param()[1][0]))
 
 
 
-def test_sequential(opt=Optimizer.ADAM(lr=0.001)):
+def test_sequential(opt):
+    """
+    This method tests sequential method using MSE loss and takes optimizer as a parameter
+    """
     
+    # train data
     TRAIN_FEATURES = torch.empty(1000, 2).uniform_(0, 1)
+    # get labels depending if in or outside of the circle
     TRAIN_LABELS = h.get_labels(TRAIN_FEATURES, torch.empty(1, 2).fill_(0.5))
     
+    # test data
     TEST_FEATURES = torch.empty(1000, 2).uniform_(0, 1)
+    # get labels for the test set
     TEST_LABELS   = h.get_labels(TEST_FEATURES, torch.empty(1, 2).fill_(0.5)) 
 
     
     plt.figure()
     h.plot_points(TRAIN_FEATURES, TRAIN_LABELS, "Training Data with Labels")
     
+    # create a sequential model with 3 Linear layers, firts 2 with Relu and tha last with
+    # Tanh activation function
     model = Module.Sequential(Module.Linear(2, 25),
                               Module.ReLU(),
-                              #Module.Dropout(p=.5),
                               Module.Linear(25, 25),
                               Module.ReLU(),
-                              #Module.Dropout(p=.5),
                               Module.Linear(25, 2),
                               Module.Tanh())
 
+    # use MSE Loss
     loss_fun = Loss.MSE()
-    
-    #opt = Optimizer.ADAM(lr=0.001)
-    
+        
+    # train model fr 200 epochs for the given optimizer with batch size of 10
     model.train(TRAIN_FEATURES, TRAIN_LABELS, epochs=200, batch_size=10, opt=opt, 
                 loss=loss_fun, verbose=True, accuracy=True, 
                 validation_set=(TEST_FEATURES, TEST_LABELS))
 
+    
+    # predict labels on a test set
     test_labels = model.predict(TEST_FEATURES)
     
     plt.figure()
@@ -72,96 +90,137 @@ def test_sequential(opt=Optimizer.ADAM(lr=0.001)):
     
 
 def get_activation(ReLU):
-    
+    """
+    Returns a new ReLU module if parameter is true, otherwise Tanh
+    """
     if ReLU:
         return Module.ReLU()
     else:
         return Module.Tanh()
     
     
-def test_softmax(dropout=False, opt=Optimizer.ADAM(lr=0.001), ReLu=True, nb_neuron=25):
+def test_softmax(opt, dropout=False, ReLu=True, nb_neuron=25):
+    """
+    Learns sequential model with softmax layer and cross entropy loss
     
+    :param opt:        optimizer to be used
+    :param dropout:    whether to use dropouts
+    :param ReLu:       whether to use ReLU (otherwise Tanh)
+    :param nb_neuron:  number of neurons for hidden layer
+        
+    :return:           trained model
+    """
+    
+    # train data
     TRAIN_FEATURES = torch.empty(1000, 2).uniform_(0, 1)
+    # get training labels depending if in or outside of the circle
     TRAIN_LABELS = h.get_labels(TRAIN_FEATURES, torch.empty(1, 2).fill_(0.5))
     
+    # test data
     TEST_FEATURES = torch.empty(1000, 2).uniform_(0, 1)
+    # get labels for the test set
     TEST_LABELS   = h.get_labels(TEST_FEATURES, torch.empty(1, 2).fill_(0.5)) 
     
     
     plt.figure()
     h.plot_points(TRAIN_FEATURES, TRAIN_LABELS, "Training points and labels")
   
-    
+    # create a sequential model
     model = Module.Sequential()
-    
-    #model = Module.Sequential(Module.Linear(2, 25),
-    #                          Module.ReLU(),
-    #                          #Module.Dropout(p=.5),
-    #                          Module.Linear(25, 25),
-    #                          Module.ReLU(),
-    #                          #Module.Dropout(p=.5),
-    #                         Module.Linear(25, 2))
 
-
+    # add linear layer with ReLu or Tanh activation depending on a ReLU parameter
     model.add(Module.Linear(2, nb_neuron))
     model.add(get_activation(ReLu))
       
 
     if dropout:
+        # add dropouts in case dropout=True with p value of 0.5
         model.add(Module.Dropout(p=.5))
-        
+      
+    # add hidden layer with nb_neuron neurons followed by an activation
     model.add(Module.Linear(nb_neuron, nb_neuron))
     model.add(get_activation(ReLu))
     
     if dropout:
         model.add(Module.Dropout(p=.5))
         
+    # add final linear layer
     model.add(Module.Linear(nb_neuron, 2))
 
+    # crossentropy loss combines with the final softmax layer
     loss_ce = Loss.CrossEntropy();
     
+    # train the model for the given parameters + 200 epochs, 10 batch size
     model.train(TRAIN_FEATURES, TRAIN_LABELS, epochs=200, batch_size=10, opt=opt, 
                 loss=loss_ce, verbose=True, accuracy=True, 
                 validation_set=(TEST_FEATURES, TEST_LABELS))
-                    
+     
+    # predict outcome of the test data               
     test_labels = model.predict(TEST_FEATURES)
 
     h.plot_points(TEST_FEATURES, test_labels, "Test points and predictions")
     return model
+   
     
+###########################################_______#########################################
+##########################################| TEST |#########################################
+##########################################|______|#########################################
+
 #test linear model 
-#test_linear()
+print("Training Linear Model")
+test_linear()
 
+#MSE and CrossEntropy comparison + plot
+print("Training Sequential NN with MSE Loss")
+MSE_model = test_sequential(opt=Optimizer.ADAM(lr=0.001))
+print("Training Sequential NN with Cross Entropy Loss")
+Cross_entropy_model = test_softmax(opt=Optimizer.ADAM(lr=0.001))
 
-#MSE_model = test_sequential()
-#Cross_entropy_model = test_softmax()
-
-"""fig = h.comparison_plot(MSE_model.history,
+fig = h.comparison_plot(MSE_model.history,
                         Cross_entropy_model.history,
                         label1="MSE",
                         label2="Cross Entropy",
-                        title="MSE vs Cross Entropy",
-                        fig_size=(5, 15))
-"""
+                        title="MSE and Cross Entropy",
+                        fig_size=(4, 10))
 
-#ReLu_model = test_softmax()
-#Tanh_model = test_softmax(ReLu=False)
 
-"""fig = h.comparison_plot(ReLu_model.history,
+#Relu and Tanh comparison + plot
+print("Training Sequential NN with ReLU as activation function")
+ReLu_model = test_softmax(opt=Optimizer.ADAM(lr=0.001))
+print("Training Sequential NN with Tanh as activation function")
+Tanh_model = test_softmax(opt=Optimizer.ADAM(lr=0.001), ReLu=False)
+
+fig = h.comparison_plot(ReLu_model.history,
                         Tanh_model.history,
                         label1="ReLU",
                         label2="Tanh",
                         title="Neural Network with ReLU vs Tanh",
-                        fig_size=(5, 15))
-"""
+                        fig_size=(4, 10))
 
-Nodrop_model  = test_softmax(nb_neuron=500)
-Yesdrop_model = test_softmax(nb_neuron=500, dropout=True)
+
+#with and without dropout comparison + plot
+print("Training Sequential NN with CE loss and without dropouts")
+Nodrop_model  = test_softmax(opt=Optimizer.ADAM(lr=0.001), nb_neuron=500)
+print("Training Sequential NN with CE loss and dropouts(p=0.5)")
+Yesdrop_model = test_softmax(opt=Optimizer.ADAM(lr=0.001), nb_neuron=500, dropout=True)
 
 fig = h.comparison_plot(Nodrop_model.history,
                         Yesdrop_model.history,
                         label1="No Dropout",
                         label2="Dropout",
                         title="Neural Network with and without dropouts",
-                        fig_size=(3, 8))
+                        fig_size=(4, 10))
+
+#ADAM and SGD comparison + plot
+print("Training Sequential NN with SGD")
+SGD_model  = test_softmax(opt=Optimizer.SGD(lr=0.1))
+print("Training Sequential NN with ADAM optimizer")
+ADAM_model = test_softmax(Optimizer.ADAM(lr=0.0001))
+
+fig = h.comparison_plot(SGD_model.history,
+                        ADAM_model.history,
+                        label1="SGD",
+                        label2="ADAM",
+                        title="Neural Network with SGD vs ADAM",
+                        fig_size=(4, 10))
 
